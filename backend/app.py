2fb062ee403ac00289af5bbf8caaf76f523c58e2
Hieu <oneraix113@gmail.com>
2025-06-07 11:30:22 +0700

    chinh sua app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from config import Config
-from flask_cors import CORS 
-from flask import send_from_directory
+from flask_cors import CORS
 
 
 def create_app():
@@ -12,14 +11,17 @@ def create_app():
     from routes.collage import collage_bp
     app.register_blueprint(collage_bp, url_prefix='/api')
 
-    #Thêm route phục vụ ảnh trong static/collages
+    # Thêm route phục vụ ảnh trong static/collages
     @app.route('/static/collages/<path:filename>')
     def serve_collage(filename):
         return send_from_directory('static/collages', filename)
 
     return app
 
+
+# Cho flask run sử dụng
 app = create_app()
 
+# Cho phép chạy bằng python app.py
 if __name__ == '__main__':
     app.run(debug=True)
